cdf31d74a92a589beb53df5ff5bcbed12a674774
kshamata Gotekar <kshamata9dec@gmail.com>
2026-06-29 16:56:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [displayVal, setDisplayVal] = React.useState("");
+
+  const handleAppend = (alphanum) => {
+    setDisplayVal(displayVal + alphanum);
+  };
+  const clear = () => {
+    setDisplayVal("");
+  };
+
+  const calculator = () => {
+    //eslint-disable-next-line no-eval
+    const result = eval(displayVal);
+    setDisplayVal(result);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Calculator</h1>
+      <div className="screendisplay">{displayVal}</div>
+      <div style={{ display: "flex" }}>
+        <div>
+          {[...Array(9)].map((_, i) => (
+            <button
+              className="btn"
+              key={i + 1}
+              onClick={() => handleAppend(i + 1)}
+            >
+              {i + 1}
+            </button>
+          ))}
+          <button className="btn" onClick={() => handleAppend("+")}>
+            +
+          </button>
+          <button className="btn" onClick={() => handleAppend("-")}>
+            -
+          </button>
+          <button className="btn" onClick={() => handleAppend("/")}>
+            /
+          </button>
+        </div>
+        <div>
+          <button className="btn" onClick={() => handleAppend(0)}>
+            0
+          </button>
+          <button className="btn" onClick={clear}>
+            c
+          </button>
+          <button className="btn" onClick={calculator}>
+            =
+          </button>
+          <button className="btn" onClick={() => handleAppend("*")}>
+            x
+          </button>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 }
